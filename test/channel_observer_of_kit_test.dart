6b8cb5aa6f33cb7b9b42452e18b4885e0b8f3647
bladeofgod <450103187@qq.com>
2022-05-04 10:41:35 +0800

    feat: 增加测试用例

--- test/channel_observer_of_kit_test.dart
+++ test/channel_observer_of_kit_test.dart
@@ -1,5 +1,20 @@
 
+import 'package:channel_observer_of_kit/channel_observer_of_kit.dart';
+import 'package:clock/src/clock.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+
+  const OptionalMethodChannel channel = OptionalMethodChannel('channel_observer_of_kit');
+
+  ChannelObserverBinding.ensureInitialized() as ChannelObserverBinding;
+
+  channel.invokeMethod('getPlatformVersion');
+
+  test('testCustomChannelBinding', () async {
+    expect(ChannelObserverOfKit.getBindingInstance()?.popChannelRecorders().length
+        , inInclusiveRange(1, 20));
+  });
 
 }
